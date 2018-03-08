b13c343206cc906d29dbae678b74e5963f8d0b13
sskillman <s.skillman@surrey.ac.uk>
2018-03-08 15:10:53 +0000

    package

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
-setup(name='swenergenie', version='0.1', packages=['swenergenie'])
+setup(name='swenergenie', version='0.1', packages=find_packages())
