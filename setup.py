19f6e11726363ec112cd72f8da1700496c6274a2
sskillman <s.skillman@surrey.ac.uk>
2018-03-08 15:16:33 +0000

    package

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
-from setuptools import setup, find_packages
+from setuptools import setup
 
-setup(name='swenergenie', version='0.1', packages=find_packages())
+setup(name='swenergenie', version='0.1', packages=['swenergenie'], package_data={'swenergenie': ['swenergenie/drv/*']}, include_package_data=True,)
