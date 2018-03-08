138952291272a04b603bb1b2cdd09c5d1b744a7b
sskillman <s.skillman@surrey.ac.uk>
2018-03-08 15:19:32 +0000

    package

--- setup.py
+++ setup.py
@@ -1,3 +1,3 @@
 from setuptools import setup
 
-setup(name='swenergenie', version='0.1', packages=['swenergenie'], package_data={'swenergenie': ['drv/*']}, include_package_data=True,)swenergenie
+setup(name='swenergenie', version='0.1', packages=['swenergenie'], package_data={'swenergenie': ['drv/*']}, include_package_data=True)
